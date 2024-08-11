b25a16444f8121789d3ca45151d3b33d3b22dd8e
hhsaputraa <hhsptr12@gmail.com>
2024-08-11 16:37:59 +0700

    add routes for company page

--- app/(dashboard)/(routes)/admin/companies/create/page.tsx
+++ app/(dashboard)/(routes)/admin/companies/create/page.tsx
@@ -1,5 +1,83 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { FormControl, FormField, FormItem, FormLabel, Form, FormDescription, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+const formSchema = z.object({
+  // ganti text munculkan jika judul kerjaan tidak diisi
+  name: z.string().min(1, { message: "Company name cannot be empty" }),
+});
+
 const CompanyCreatePage = () => {
-  return <div>tes</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+  const router = useRouter();
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/companies", values);
+      router.push(`/admin/companies/${response.data.id}`);
+      toast.success("Company name Created");
+    } catch (error) {
+      console.log((error as Error)?.message);
+      toast.error((error as Error)?.message);
+      //toast notif
+    }
+  };
+
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div>
+        <h1 className="text-2xl">Nama Perusahaan</h1>
+        <p className="text-sm text-neutral-500"> what would you like to name your company? Don&apos;t worry you can change this later</p>
+        {/* form */}
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mt-8">
+            {/* form field */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company name</FormLabel>
+                  <FormControl>
+                    <Input disabled={isSubmitting} placeholder="PT bongkar turret" {...field} />
+                  </FormControl>
+                  <FormDescription>name for this company</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href={"/admin/jobs"}>
+                <Button type="button" variant={"ghost"}>
+                  Cancel
+                </Button>
+              </Link>
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default CompanyCreatePage;
